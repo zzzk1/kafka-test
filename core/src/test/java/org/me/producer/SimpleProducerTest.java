d9ecee0ca1b57988c7d1210cfd055c0aac4785a9
zzzk1 <madzhou1@gmail.com>
2024-10-21 10:20:53 +0800

    core: send custom object value

--- core/src/test/java/org/me/producer/SimpleProducerTest.java
+++ core/src/test/java/org/me/producer/SimpleProducerTest.java
@@ -3,71 +3,100 @@ package org.me.producer;
 import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.CreateTopicsResult;
-import org.apache.kafka.clients.admin.DeleteTopicsResult;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 
 public class SimpleProducerTest {
-    private static Producer<String, String> producer;
+    private static Producer<String, Object> producer;
     private static Admin admin;
 
     @BeforeAll
+    static void init() {
+        initProducer();
+        initAdmin();
+        setUpTopic();
+    }
+
     static void initProducer() {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, io.confluent.kafka.serializers.KafkaJsonSerializer.class.getName());
         producer = new KafkaProducer<>(props);
     }
 
-    @BeforeAll
     static void initAdmin() {
         Properties props = new Properties();
         props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         admin = AdminClient.create(props);
     }
 
-    @BeforeEach
-    void setUp() throws ExecutionException, InterruptedException {
+    static void setUpTopic() {
         final String topic = "simple-topic";
         final int partition = 1;
         final short replication = 1;
-        CreateTopicsResult result = admin.createTopics(
+        admin.createTopics(
                 Collections.singleton(
                         new NewTopic(topic, partition, replication)));
-        result.all().get();
     }
 
-    @AfterEach
-    void clearUp() throws ExecutionException, InterruptedException {
+    @AfterAll
+    static void clearUp() {
         final String topic = "simple-topic";
-        DeleteTopicsResult result = admin.deleteTopics(Collections.singleton(topic));
-        result.all().get();
+        admin.deleteTopics(Collections.singleton(topic));
     }
 
     @Test
     void testSimpleSend() {
         String topic = "simple-topic";
         String message = "Hello World";
-        ProducerRecord<String, String> record = new ProducerRecord<>(topic, message);
+        ProducerRecord<String, Object> record = new ProducerRecord<>(topic, message);
         producer.send(record, (recordMetadata, e) -> {
+            assertNull(e);
             assertEquals(recordMetadata.topic(), topic);
         });
+    }
+
+    @Test
+    void testSendWithPartition() {
+        final String topic = "simple-topic";
+        final String key = "foo";
+        final String value = "bar";
+        final int partition = 0;
+        ProducerRecord<String, Object> record = new ProducerRecord<>(topic, partition, key, value);
+        producer.send(record, (recordMetadata, e) -> {
+            assertNull(e);
+            assertEquals(recordMetadata.topic(), topic);
+            assertEquals(recordMetadata.partition(), partition);
+        });
+    }
+
+    @Test
+    void testSendWithCustomObject() {
+        final String topic = "simple-topic";
+        final Foo foo = new Foo("foo", 1);
+
+        ProducerRecord<String, Object> record = new ProducerRecord<>(topic, foo);
+        producer.send(record, (recordMetadata, e) -> {
+            assertNull(e);
+            assertEquals(recordMetadata.topic(), topic);
+        });
+    }
+
+    record Foo(String name, int id) {
     }
 }
